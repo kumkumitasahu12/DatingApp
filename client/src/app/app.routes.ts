fd98bb1fefcaeccc50890bd23e11be0e17b1c80f
Kumkumita Sahu <kumkumitasahu12@gmail.com>
2024-09-05 16:05:28 +0200

    added angular routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberDetailComponent } from './memebers/member-detail/member-detail.component';
+import { MemberListComponent } from './memebers/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: '', runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children:[
+            {path: 'members/:id', component: MemberDetailComponent},
+    {path: 'members', component: MemberListComponent, canActivate: [authGuard]},
+    {path: 'lists', component: ListsComponent},
+    {path: 'messages', component: MessagesComponent}
+        ]
+    },
+    {path: '**', component: HomeComponent,pathMatch:'full'}
+];
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,26 +2,32 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, TitleCasePipe, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 accountService = inject(AccountService);
+private route = inject(Router)
+private toastr = inject(ToastrService);
   model: any ={};
 login() {
   this.accountService.login(this.model).subscribe({
-    next: response => {
-      console.log(response)
+    next: () => {
+      this.route.navigateByUrl('/members')
     },
-    error: error => console.log(error)
+    error: error => this.toastr.error(error)
   })
 }
 logout() {
   this.accountService.logout()
+  this.route.navigateByUrl('');
   }
 }
